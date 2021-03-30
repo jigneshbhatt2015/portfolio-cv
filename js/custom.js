d98cc4a8578cc33aa5fa8e7a302771a660ae474a
Prashant Padmanabhan <prashantpro@users.noreply.github.com>
2021-03-30 17:27:35 +0530

    Update custom.js
    Fixed error message

--- js/custom.js
+++ js/custom.js
@@ -107,17 +107,12 @@
 				method: "POST",
 				data: form.serialize(),
 				success: function(result) {
-					if (result == "success") {
-						$(".form-inputs").css("display", "none");
-						$(".box p").css("display", "none");
-						$(".contactform").find(".output_message").addClass("success");
-						$(".output_message").text("Message Sent!");
-					} else {
-						$(".tabs-container").css("height", "440px");
-
-						$(".contactform").find(".output_message").addClass("error");
-						$(".output_message").text("Error Sending!");
-					}
+					
+					$(".form-inputs").css("display", "none");
+					$(".box p").css("display", "none");
+					$(".contactform").find(".output_message").addClass("success");
+					$(".output_message").text("Message Sent!");
+					
 				}
 			});
 
