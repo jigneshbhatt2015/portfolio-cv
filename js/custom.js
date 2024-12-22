34279478d68fb7b071f3d2e109b86a182c002973
Jignesh Bhatt <jigneshbhatt2015@gmail.com>
2024-12-22 23:14:16 +0530

    Updated HTML content

--- js/custom.js
+++ js/custom.js
@@ -1,140 +1,174 @@
 (function($) {
+    "use strict";
 
-	"use strict";
-
-	/* ----------------------------------------------------------- */
-	/*  FUNCTION TO STOP LOCAL AND YOUTUBE VIDEOS IN SLIDESHOW
+    /* ----------------------------------------------------------- */
+    /*  FUNCTION TO STOP LOCAL AND YOUTUBE VIDEOS IN SLIDESHOW
     /* ----------------------------------------------------------- */
 
-	function stop_videos() {
-		var video = document.getElementById("video");
-		if (video.paused !== true && video.ended !== true) {
-			video.pause();
-		}
-		$('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-	}
+    function stop_videos() {
+        // Stop local video if it's playing
+        var video = document.getElementById("video");
+        if (video && video.paused !== true && video.ended !== true) {
+            video.pause();
+        }
 
-	$(document).ready(function() {
+        // Check if the YouTube iframe exists before trying to stop the video
+        var youtubeVideo = $('.youtube-video')[0];
+        if (youtubeVideo) {
+            var contentWindow = youtubeVideo.contentWindow;
+            if (contentWindow) {
+                contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+            }
+        }
+    }
 
-		/* ----------------------------------------------------------- */
-		/*  STOP VIDEOS
+    $(document).ready(function() {
+
+        /* ----------------------------------------------------------- */
+        /*  STOP VIDEOS
         /* ----------------------------------------------------------- */
 
-		$('.slideshow nav span').on('click', function () {
-			stop_videos();
-		});
+        $('.slideshow nav span').on('click', function () {
+            stop_videos();
+        });
 
-		/* ----------------------------------------------------------- */
-		/*  FIX REVEALATOR ISSUE AFTER PAGE LOADED
+        /* ----------------------------------------------------------- */
+        /*  FIX REVEALATOR ISSUE AFTER PAGE LOADED
         /* ----------------------------------------------------------- */
 
-		$(".revealator-delay1").addClass('no-transform');
+        $(".revealator-delay1").addClass('no-transform');
 
-		/* ----------------------------------------------------------- */
-		/*  PORTFOLIO GALLERY
+        /* ----------------------------------------------------------- */
+        /*  PORTFOLIO GALLERY
         /* ----------------------------------------------------------- */
 
-		if ($('.grid').length) {
-			new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
-		}
+        // Initialize CBPGridGallery only once
+        var gridGalleryInitialized = false;
 
-		/* ----------------------------------------------------------- */
-		/*  BUTTONS ANIMATION
+        function initializeCBPGridGallery() {
+            if ($('#grid-gallery').length && !gridGalleryInitialized) {
+                try {
+                    new CBPGridGallery(document.getElementById('grid-gallery'));
+                    gridGalleryInitialized = true;  // Mark as initialized
+                } catch (e) {
+                    console.error('Error initializing CBPGridGallery:', e);
+                }
+            }
+        }
+
+        initializeCBPGridGallery(); // Call once on page load
+
         /* ----------------------------------------------------------- */
-		function checkSize() {
-			if ($( document ).width() > 992) {
-				var btn_hover = "";
-				$(".btn").each(function() {
-					var btn_text = $(this).text();
-					$(this).addClass(btn_hover).empty().append("<span data-hover='" + btn_text + "'>" + btn_text + "</span>");
-				});
-			}
-		}
-		checkSize();
-		window.addEventListener('resize', function () {
-			checkSize();
-		});
-
-		/* ----------------------------------------------------------- */
-		/*  HIDE HEADER WHEN PORTFOLIO SLIDESHOW OPENED
+        /*  BUTTONS ANIMATION
         /* ----------------------------------------------------------- */
+        function checkSize() {
+            if ($( document ).width() > 992) {
+                var btn_hover = "";
+                $(".btn").each(function() {
+                    var btn_text = $(this).text();
+                    $(this).addClass(btn_hover).empty().append("<span data-hover='" + btn_text + "'>" + btn_text + "</span>");
+                });
+            }
+        }
+        checkSize();
+        window.addEventListener('resize', function () {
+            checkSize();
+        });
 
-		$(".grid figure").on('click', function() {
-			$("#navbar-collapse-toggle").addClass('hide-header');
-		});
-
-		/* ----------------------------------------------------------- */
-		/*  SHOW HEADER WHEN PORTFOLIO SLIDESHOW CLOSED
         /* ----------------------------------------------------------- */
-
-		$(".nav-close").on('click', function() {
-			$("#navbar-collapse-toggle").removeClass('hide-header');
-		});
-		$(".nav-prev").on('click', function() {
-			if ($('.slideshow ul li:first-child').hasClass('current')) {
-				$("#navbar-collapse-toggle").removeClass('hide-header');
-			}
-		});
-		$(".nav-next").on('click', function() {
-			if ($('.slideshow ul li:last-child').hasClass('current')) {
-				$("#navbar-collapse-toggle").removeClass('hide-header');
-			}
-		});
-
-		/* ----------------------------------------------------------- */
-		/*  PORTFOLIO DIRECTION AWARE HOVER EFFECT
+        /*  HIDE HEADER WHEN PORTFOLIO SLIDESHOW OPENED
         /* ----------------------------------------------------------- */
 
-		var item = $(".grid li figure");
-		var elementsLength = item.length;
-		for (var i = 0; i < elementsLength; i++) {
-			$(item[i]).hoverdir();
-		}
+        $(".grid figure").on('click', function() {
+            // Reset current class for all gallery items
+            $(".grid figure").removeClass('current');
 
-		/* ----------------------------------------------------------- */
-		/*  AJAX CONTACT FORM
+            // Add current class to clicked item
+            $(this).addClass('current');
+            
+            // Hide the header when slideshow opens
+            $("#navbar-collapse-toggle").addClass('hide-header');
+            $(".slideshow").fadeIn();  // Show the slideshow popup
+
+            // Initialize the CBPGridGallery if it's not initialized yet
+            initializeCBPGridGallery();
+        });
+
+        /* ----------------------------------------------------------- */
+        /*  SHOW HEADER WHEN PORTFOLIO SLIDESHOW CLOSED
         /* ----------------------------------------------------------- */
 
+        $(".nav-close").on('click', function() {
+            $("#navbar-collapse-toggle").removeClass('hide-header');
+            $(".slideshow").fadeOut(); // Fade out the slideshow container
 
+            // Ensure the grid is reset (remove the current class)
+            $(".grid figure").removeClass('current');
+        });
 
+        $(".nav-prev").on('click', function() {
+            if ($('.slideshow ul li:first-child').hasClass('current')) {
+                $("#navbar-collapse-toggle").removeClass('hide-header');
+            }
+        });
 
-		$(".contactform").on("submit", function() {
-			$(".output_message").text("Message Sent!");
+        $(".nav-next").on('click', function() {
+            if ($('.slideshow ul li:last-child').hasClass('current')) {
+                $("#navbar-collapse-toggle").removeClass('hide-header');
+            }
+        });
 
-			var form = $(this);
-			$.ajax({
-				url: "https://getform.io/f/9b5abe9f-977e-4fb2-878c-39c466d36361",
-				method: "POST",
-				data: form.serialize(),
-				success: function(result) {
-					
-					$(".form-inputs").css("display", "none");
-					$(".box p").css("display", "none");
-					$(".contactform").find(".output_message").addClass("success");
-					$(".output_message").text("Message Sent!");
-					
-				}
-			});
-
-			return false;
-		});
-
-	});
-
-	$(document).keyup(function(e) {
-
-		/* ----------------------------------------------------------- */
-		/*  KEYBOARD NAVIGATION IN PORTFOLIO SLIDESHOW
         /* ----------------------------------------------------------- */
-		if (e.keyCode === 27) {
-			stop_videos();
-			$('.close-content').click();
-			$("#navbar-collapse-toggle").removeClass('hide-header');
-		}
-		if ((e.keyCode === 37) || (e.keyCode === 39)) {
-			stop_videos();
-		}
-	});
+        /*  PORTFOLIO DIRECTION AWARE HOVER EFFECT
+        /* ----------------------------------------------------------- */
 
+        var item = $(".grid li figure");
+        var elementsLength = item.length;
+        for (var i = 0; i < elementsLength; i++) {
+            $(item[i]).hoverdir();
+        }
+
+        /* ----------------------------------------------------------- */
+        /*  AJAX CONTACT FORM
+        /* ----------------------------------------------------------- */
+
+        $(".contactform").on("submit", function() {
+            $(".output_message").text("Message Sent!");
+
+            var form = $(this);
+            $.ajax({
+                url: "https://getform.io/f/9b5abe9f-977e-4fb2-878c-39c466d36361",
+                method: "POST",
+                data: form.serialize(),
+                success: function(result) {
+
+                    $(".form-inputs").css("display", "none");
+                    $(".box p").css("display", "none");
+                    $(".contactform").find(".output_message").addClass("success");
+                    $(".output_message").text("Message Sent!");
+
+                }
+            });
+
+            return false;
+        });
+
+    });
+
+    $(document).keyup(function(e) {
+
+        /* ----------------------------------------------------------- */
+        /*  KEYBOARD NAVIGATION IN PORTFOLIO SLIDESHOW
+        /* ----------------------------------------------------------- */
+        if (e.keyCode === 27) {
+            stop_videos();
+            $('.close-content').click();
+            $("#navbar-collapse-toggle").removeClass('hide-header');
+            $(".slideshow").fadeOut(); // Close the slideshow when Escape key is pressed
+        }
+        if ((e.keyCode === 37) || (e.keyCode === 39)) {
+            stop_videos();
+        }
+    });
 
 })(jQuery);
